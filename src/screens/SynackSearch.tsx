092c5724276e97bafd83680a5cfbcaec6b65da18
carlosrodriguezheylaikacom <calosrodriguez@heylaika.com>
2022-07-11 18:15:23 -0300

    Create seach list

--- src/screens/SynackSearch.tsx
+++ src/screens/SynackSearch.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Search } from '../components/Search'
+import { SearchResult } from '../components/SearchResults'
 
 export const SynackSearch = () => <Container>
     <h1>Synac search</h1>
     <Search />
+    <SearchResult />
 </Container>
 
 const Container = styled.div`
-    margin: 50px 0 0 100px;
+    margin: 40px 0 0 80px;
 `
